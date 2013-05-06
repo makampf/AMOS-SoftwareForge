bc6b4f3e8ddbb496d187407f9a3215982cd096bf
Denis Bach <dbathlon@arcor.de>
2013-05-06 21:56:59 +0200

    Added Comments for HomeController.cs and TeamCollections.cs

--- SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/HomeController.cs
+++ SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
 using System.Web.Mvc;
 using SoftwareForge.Common.Models;
 using SoftwareForge.Mvc.WebApiClient;
 
 namespace SoftwareForge.Mvc.Controllers
 {
+    /// <summary>
+    /// Controller for home view.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// GET: /Home.
+        /// </summary>
+        /// <returns>Home view with teamcollections.</returns>
         public ActionResult Index()
         {
             IEnumerable<TeamCollection> teamCollections = TeamCollectionsClient.GetTeamCollections();
--- SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/TeamCollectionsController.cs
+++ SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/TeamCollectionsController.cs
@@ -4,13 +4,25 @@ using SoftwareForge.Mvc.WebApiClient;
 
 namespace SoftwareForge.Mvc.Controllers
 {
+    /// <summary>
+    /// Controller for Teamcollections
+    /// </summary>
     public class TeamCollectionsController : Controller
     {
+        /// <summary>
+        /// GET: /CreateTeamCollection.
+        /// </summary>
+        /// <returns>Teamcollection view for the browser</returns>
         public ActionResult CreateTeamCollection()
         {
             return View();
         }
-
+        /// <summary>
+        /// POST: /CreateTeamCollection with a message body.
+        /// </summary>
+        /// <param name="collection">Message body <example> name1=value1&name2=value2 </example></param>
+        /// <returns>Either view with new collection, returns to the start page or view where 
+        /// the user have to type the name of the colection. </returns>
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult CreateTeamCollection(FormCollection collection)
         {
