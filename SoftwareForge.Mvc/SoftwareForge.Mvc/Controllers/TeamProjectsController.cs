a3777c7ee1f42b8f31f0ec214262918be2b0c011
Florian Wittmann <git@florianwittmann.de>
2013-05-16 21:46:25 +0200

    Added Dashboard to menu

--- SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/TeamProjectsController.cs
+++ SoftwareForge.Mvc/SoftwareForge.Mvc/Controllers/TeamProjectsController.cs
@@ -52,5 +52,31 @@ namespace SoftwareForge.Mvc.Controllers
             Project teamCollections = TeamCollectionsClient.GetTeamProject(guid);
             return View(teamCollections);
         }
+
+        /// <summary>
+        /// Join a project
+        /// </summary>
+        /// <param name="guid">guid of project</param>
+        /// <param name="username">username</param>
+        /// <returns>Redirects to overview page</returns>
+        public ActionResult JoinProject(Guid guid, String username)
+        {
+
+            TeamCollectionsClient.JoinProject(guid, username);
+            return RedirectToAction("ProjectDetailsPage",new {guid});
+
+        }
+
+        /// <summary>
+        /// Leave a project
+        /// </summary>
+        /// <param name="guid">guid of project</param>
+        /// <param name="username">username</param>
+        /// <returns>Redirects to overview page</returns>
+        public ActionResult LeaveProject(Guid guid, String username)
+        {
+            TeamCollectionsClient.LeaveProject(guid, username);
+            return RedirectToAction("ProjectDetailsPage", new { guid });
+        }
     }
 }
