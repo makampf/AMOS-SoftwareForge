b698d5bf1ffd36d4504a9efe27d21e0b62eef884
Denis Bach <dbathlon@arcor.de>
2013-06-11 11:54:43 +0200

    preparation for client side implementation of [us#68]

---
+++ SoftwareForge.Common/SoftwareForge.Common/Models/ProjectInvitationMessageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareForge.Common.Models
+{
+    public class ProjectInvitationMessageModel
+    {
+        /// <summary>
+        /// The message model.
+        /// </summary>
+        public Message Message { get; set; }
+
+
+        /// <summary>
+        /// The projectJoinRequest model.
+        /// </summary>
+        public ProjectInvitationRequest ProjectInvitationRequest { get; set; }
+    }
+}
---
+++ SoftwareForge.Common/SoftwareForge.Common/Models/Requests/ProjectInvitationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareForge.Common.Models.Requests
+{
+    class ProjectInvitationModel
+    {
+        /// <summary>
+        /// the projectguid
+        /// </summary>
+        public Guid ProjectGuid { get; set; }
+
+
+        /// <summary>
+        /// The username.
+        /// </summary>
+        public String Username { get; set; }
+
+
+        /// <summary>
+        /// The userrole.
+        /// </summary>
+        public UserRole UserRole { get; set; }
+    }
+}
